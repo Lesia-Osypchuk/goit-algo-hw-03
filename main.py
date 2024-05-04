9b08b80f12c5d1a7d8e6e64919b9a2c0276194bf
Lesia-Osypchuk <155398902+Lesia-Osypchuk@users.noreply.github.com>
2024-05-04 17:49:36 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,31 +2,31 @@ import os
 import shutil
 import argparse
 
-def copy_files(source_dir, dest_dir):
-    for root, dirs, files in os.walk(source_dir):
-        for file in files:
-            source_path = os.path.join(root, file)
-            extension = os.path.splitext(file)[1][1:]
-            dest_subdir = os.path.join(dest_dir, extension)
+def copy_files(source_path, dest_dir):
+    extension = os.path.splitext(source_path)[1][1:]
+    dest_subdir = os.path.join(dest_dir, extension)
 
-            # Перевірка, чи існує піддиректорія, якщо ні - створити
-            if not os.path.exists(dest_subdir):
-                os.makedirs(dest_subdir)
+    # Перевірка, чи існує піддиректорія, якщо ні - створити
+    if not os.path.exists(dest_subdir):
+        os.makedirs(dest_subdir)
 
-            dest_path = os.path.join(dest_subdir, file)
-            try:
-                shutil.copy2(source_path, dest_path)
-                print(f"Copied {source_path} to {dest_path}")
-            except Exception as e:
-                print(f"Error copying {source_path}: {e}")
+    dest_path = os.path.join(dest_subdir, os.path.basename(source_path))
+
+    # Перевірка, чи файл уже був скопійований
+    if not os.path.exists(dest_path):
+        try:
+            shutil.copy2(source_path, dest_path)
+            print(f"Copied {source_path} to {dest_path}")
+        except Exception as e:
+            print(f"Error copying {source_path}: {e}")
 
 def recursive_copy(source_dir, dest_dir):
-    for item in os.listdir(source_dir):
-        item_path = os.path.join(source_dir, item)
-        if os.path.isdir(item_path):
-            recursive_copy(item_path, dest_dir)
-        elif os.path.isfile(item_path):
-            copy_files(source_dir, dest_dir)
+    copied_files = set()  # відслідковування скопійованих файлів
+    for root, _, files in os.walk(source_dir):
+        for file in files:
+            source_path = os.path.join(root, file)
+            copy_files(source_path, dest_dir)
+            copied_files.add(source_path)
 
 def main():
     parser = argparse.ArgumentParser(description='Recursive file copying and sorting.')
